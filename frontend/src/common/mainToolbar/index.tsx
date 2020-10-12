4305929e0c4e3f9e4978ae293686a3a1760850ec
Chris Pastor <cpasto1@students.towson.edu>
2020-10-12 19:43:23 -0400

    add poc page setup

---
+++ frontend/src/common/mainToolbar/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Button} from 'primereact/button';
+import {Toolbar} from "primereact/toolbar";
+import {Link} from 'react-router-dom';
+
+const left = () => (
+    <React.Fragment>
+        <Link to="/login">
+            <Button label="Login" className="p-mr-2" />
+        </Link>
+        <Link to="/home">
+            <Button label="Home" className="p-mr-2" />
+        </Link>
+        <Link to="/some-other-page">
+            <Button label="Some Other Page" className="p-button-link" />
+        </Link>
+    </React.Fragment>
+);
+
+const MyToolbar: React.FunctionComponent = () => (
+    <React.Fragment>
+        <Toolbar left={left} />
+    </React.Fragment>
+);
+
+
+export default MyToolbar;
