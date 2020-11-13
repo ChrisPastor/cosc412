209a56a6ffbea3efd6eef011b5fb50fd1e80876e
sjohns98 <70716155+sjohns98@users.noreply.github.com>
2020-11-12 22:13:20 -0500

    Update Extrafile.ts

--- backend/src/database/Extrafile.ts
+++ backend/src/database/Extrafile.ts
@@ -3,17 +3,22 @@
 export const updateManyWrappers: MongoCRUDFunction = async (db, otherArgs ) => {
     const {collection, filter, data} = otherArgs;
     try {
-        const result = await db.collection(collection).updateMany(filter,data);
-
-        console.log(`update was successful for: ${result.value}`);
+        if (typeof data !== "object") {
+            const result = await db.collection(collection).updateMany(filter, data);
+            
+            console.log(`update was successful for: ${result.value}`);
+        } else {
+            throw new Error(`Expected array of objects, got ${data}`);
+        }
     } catch (error) {
         console.log(`there was an error: ${error}`);
     }
+
 }
-export const replaceOneWrapper: MongoCRUDFunction = async (db, otherArgs ) => {
+export const updateOneWrapper: MongoCRUDFunction = async (db, otherArgs ) => {
     const {collection, filter, data} = otherArgs;
     try {
-        const result = await db.collection(collection).replaceOne(filter,data);
+        const result = await db.collection(collection).updateOne(filter, data);
 
         console.log(`replacement was successful for: ${result.value}`);
     } catch (error) {
