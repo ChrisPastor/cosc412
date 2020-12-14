d38661519bd39fe3c0d386ba37a56b8557251286
sjohns98 <70716155+sjohns98@users.noreply.github.com>
2020-12-13 19:10:18 -0500

    Remove testing button

--- frontend/src/common/MainToolbar/index.tsx
+++ frontend/src/common/MainToolbar/index.tsx
@@ -10,9 +10,6 @@ const left = () => (
         <Link to="/home">
             <Button label="Home" className="p-mr-2" />
         </Link>
-        <Link to="/game/solo/TestGame1">
-            <Button label="Solo Play" className="p-mr-2" />
-        </Link>
         <Donation/>
     </React.Fragment>
 );
